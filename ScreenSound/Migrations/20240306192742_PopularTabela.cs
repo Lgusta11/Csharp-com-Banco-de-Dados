4aa74abbadad39ab9af8a022951935c3ac4b3951
Lgusta20 <gugavasco2016@gmail.com>
2024-03-06 16:44:31 -0300

    Adicionando migrations

---
+++ ScreenSound/Migrations/20240306192742_PopularTabela.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ScreenSound.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopularTabela : Migration
+    {
+
+
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData("Artistas", new string[] { "Nome", "Bio" ,
+             "FotoPerfil"}, new object[] { "Djavan", "Melhor do MPB", "fotoPerfil.jpn" });
+
+            migrationBuilder.InsertData("Artistas", new string[] { "Nome", "Bio" ,
+             "FotoPerfil"}, new object[] { "Yrlan", "Fera", "fotoPerfil.jpn" });
+
+            migrationBuilder.InsertData("Artistas", new string[] { "Nome", "Bio" ,
+             "FotoPerfil"}, new object[] { "Gusta", "Mais lindo da AFS", "fotoPerfil.jpn" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
